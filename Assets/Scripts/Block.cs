8976366b4d9926726a003d18be212b45f8989e2e
Blair Lyons <blair208@gmail.com>
2016-09-17 12:16:54 -0700

    blocks are generated in area, primitive prefabs

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour 
+{
+	public Vector3 location;
+
+	public void Init (Vector3 _location)
+	{
+		location = _location;
+		transform.localPosition = location;
+		name = GetType().ToString() + "_" + location.x + ":" + location.y + ":" + location.z;
+	}
+}
---
+++ Assets/Scripts/BlockFactory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockFactory : MonoBehaviour 
+{
+	public GameObject blockPrefab;
+	public Vector3 size;
+	public Block[,,] blocks;
+
+	void Start ()
+	{
+		GenerateBlocks();
+	}
+
+	void GenerateBlocks ()
+	{
+		if (blockPrefab != null && blockPrefab.GetComponent<Block>())
+		{
+			blocks = new Block[Mathf.RoundToInt(size.x), Mathf.RoundToInt(size.y), Mathf.RoundToInt(size.z)];
+			for (int u = 0; u < size.x; u++)
+			{
+				for (int v = 0; v < size.y; v++)
+				{
+					for (int w = 0; w < size.z; w++)
+					{
+						blocks[u, v, w] = Instantiate(blockPrefab).GetComponent<Block>();
+						blocks[u, v, w].transform.SetParent(transform);
+						blocks[u, v, w].Init(new Vector3(u, v, w));
+					}
+				}
+			}
+		}
+	}
+}
