35fd1e3a07ad1d9d35785eb4046cc5e437270462
Russell Alleen-Willems <ralleenwillems@gmail.com>
2016-09-18 12:11:06 -0700

    Updating Meta Files of Assests

---
+++ Assets/Scripts/Vive/BlockPointer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockPointer : ViveController
+{
+    public LayerMask blockLayer;
+    public Block selectedBlock;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateSelectedBlock();
+	}
+
+    void UpdateSelectedBlock ()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, blockLayer))
+        {
+            Block block = hit.transform.GetComponent<Block>();
+            if (block != selectedBlock)
+            {
+                selectedBlock = block;
+            }
+        }
+        else
+        {
+            selectedBlock = null;
+        }
+    }
+}
